3967f2295abbc505564cd9afa50e9005c3f49997
Campsuns <Campsun@gmail.com>
2025-09-05 00:28:13 -0400

    Delete assets directory

--- assets/config.js
+++
@@ -1,3 +0,0 @@
-// Set this to your deployed Apps Script Web App URL, e.g.:
-// const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx.../exec";
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxH8wNUrHeWeq7jBrp-V-VzB1tS3ksaTxRIPhj1jVjjBQP35OHJzydasb990IHeQECT/exec";
